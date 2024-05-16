dd7308e2e8850313c9a802c1a7df529c51a1fdcd
PaulaAlos <107473661+PaulaAlos@users.noreply.github.com>
2024-05-16 12:52:23 +0200

    jsx

--- portfolio-cms-reactwp/src/main.jsx
+++ portfolio-cms-reactwp/src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from "react-router-dom";
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
